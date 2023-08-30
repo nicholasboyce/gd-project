24de62af9196649d1437d475cd60b2d8efe57740
nicholasboyce <nicholasboyce@protonmail.com>
2023-08-30 17:14:00 -0400

    Added service worker file and used Cache API for page assets.

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const menu = document.getElementsByClassName('menu')[0]
 toggleButton.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+
+// Register service worker to control making site work offline
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register(
+        "/gd-project/sw.js",
+      )
+      .then(() => console.log("Service Worker Registered"));
+  }
+  
---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches
+        .open("data-store")
+        .then((cache) =>
+          cache.addAll([
+            "/gd-project/index.html",
+            "/gd-project/styles.css",
+            "/gd-project/script.js",
+            "/gd-project/pages/",
+            "/gd-project/pages/about.html",
+            "/gd-project/pages/register.html",
+            "/gd-project/pages/search.html",
+            "/gd-project/pages/properties.json",
+          ]),
+        ),
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
+  
+  
