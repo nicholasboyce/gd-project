713cf58c53025d67f0756c5677929710d72c5878
nicholasboyce <nicholasboyce@protonmail.com>
2024-01-13 22:03:06 -0500

    Separated out client and server code.

---
+++ server/gdproject/src/test/java/server/gdproject/GdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package server.gdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
