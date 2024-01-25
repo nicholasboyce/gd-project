d4c1c1b8d7b96c9799b61ea184893c2d122fb59e
nicholasboyce <nicholasboyce@protonmail.com>
2024-01-25 02:46:17 -0500

    Added register.html template.

--- server/gdproject/src/main/java/server/gdproject/Registration/RegistrationController.java
+++ server/gdproject/src/main/java/server/gdproject/Registration/RegistrationController.java
@@ -2,6 +2,7 @@ package server.gdproject.Registration;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,11 @@ public class RegistrationController {
     @ResponseBody
     private ResponseEntity<String> createUser(RegistrationRequest newRegistrationRequest) {
         return registrationService.register(newRegistrationRequest);
+    }
+
+    @GetMapping
+    private String register() {
+        return "register";
     }
 
 }
